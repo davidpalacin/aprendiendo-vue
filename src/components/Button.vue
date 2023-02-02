c0b01b4a62f1589237e3eba6c5f6fe7d955eadc1
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2023-02-02 17:45:24 +0100

    add text to button component

---
+++ src/components/Button.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+defineProps(['text']);
+</script>
+
+
+<template>
+  <button class="border-4 rounded border-black w-11/12">
+    {{ text }}
+  </button>
+</template>
+
+
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+import Button from './Button.vue';
+</script>
+
 <template>
   <div
     class="bg-purple-600 h-96 rounded-b-3xl flex flex-col justify-center items-center"
@@ -6,5 +10,10 @@
     <span class="text-white text-center">
       La única red social centrada en los conciertos que más te gustan
     </span>
+  </div>
+
+  <div class="flex flex-col h-full justify-center items-center mt-10 gap-3">
+    <Button text="Crear una cuenta" />
+    <Button text="Entrar con mi cuenta" />
   </div>
 </template>
