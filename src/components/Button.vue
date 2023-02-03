f9a014ad1d8bcdfae00ced052b2f6144375e78b9
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2023-02-03 22:25:32 +0100

    update

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -2,9 +2,8 @@
 defineProps(['text']);
 </script>
 
-
 <template>
-  <button class="border-4 rounded border-black w-11/12">
+  <button class="border-4 text-white text-2xl border-white w-11/12 h-28">
     {{ text }}
   </button>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,17 +3,21 @@ import Button from './Button.vue';
 </script>
 
 <template>
-  <div
-    class="bg-purple-600 h-96 rounded-b-3xl flex flex-col justify-center items-center"
-  >
-    <span class="text-white font-bold text-6xl">The Wall</span> 
-    <span class="text-white text-center">
-      La única red social centrada en los conciertos que más te gustan
-    </span>
-  </div>
-
-  <div class="flex flex-col h-full justify-center items-center mt-10 gap-3">
-    <Button text="Crear una cuenta" />
-    <Button text="Entrar con mi cuenta" />
+  <div class="bg-black h-screen sm:h-full">
+    <header
+      class="bg-purple-600 h-96 rounded-b-3xl flex flex-col justify-center items-center"
+    >
+      <span class="text-white font-bold text-6xl">The Wall</span> 
+      <span class="text-white text-center">
+        La única red social centrada en los conciertos que más te gustan
+      </span>
+    </header>
+  
+    <main class="flex flex-col justify-center items-center mt-10 gap-3">
+      <router-link to="/register">
+        <Button text="Crear una cuenta" />
+      </router-link>
+      <Button text="Entrar con mi cuenta" />
+    </main>
   </div>
 </template>
